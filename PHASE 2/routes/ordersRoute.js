0b4b0ceecc4afcedf645c75a40bee6e137cdd3fe
sophiasee <sophia_see@dlsu.edu.ph>
2020-05-12 19:42:08 +0800

    removed phase 2

--- PHASE 2/routes/ordersRoute.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-const orderController = require('../controllers/orderController')
-
-router.get('/:status', orderController.getOrderStatus);
-
-module.exports = router;
